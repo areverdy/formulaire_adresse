c7902463b90024dd53e3870f85c58db8347b8a51
areverdy <alec.rev1234@gmail.com>
2023-11-10 16:24:01 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useCallback, useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+                  <input 
+                  type="text" 
+                  placeholder="Saisir votre adresse"
+      
+              />
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+const Formulaire =() => {
+  const [ville, setVille] = useState<string[]>([])
+
+
+
+}
---
+++ src/FormulaireAdresse.tsx
@@ -0,0 +1,25 @@
+const FormulaireAdresse => {
+    const []
+
+
+
+
+
+
+
+ 
+
+    return (
+        <div className="form-address">
+            <input 
+                type="text" 
+                placeholder="Saisir votre adresse"
+            />
+
+    )
+
+}
+
+
+
+export default FormulaireAdresse
